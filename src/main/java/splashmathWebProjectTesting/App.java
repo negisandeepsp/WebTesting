1a10cfe7c132c40c52363907236320917f694fe3
Sandeep Negi <sandeep.negi@splashmath.com>
2017-04-19 13:33:22 +0530

    first commit

---
+++ src/main/java/splashmathWebProjectTesting/App.java
@@ -0,0 +1,13 @@
+package splashmathWebProjectTesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/splashmathWebProjectTesting/AppTest.java
@@ -0,0 +1,13 @@
+package splashmathWebProjectTesting;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+	@Test
+	public void fun()
+	{
+		System.out.println("Hello");
+	}
+   
+}
